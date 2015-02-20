d73b4d83a1770a986539de12d82d42f54a6b0224
graydon-armstrong <seeds77222@hotmail.com>
2015-02-19 23:17:31 -0500

    Initial Commit of Steam API Object
    First commit of the steam api object. Can set the api key and the user
    id(if its a number). Can also get a json array of a users owned games

--- steam_api.py
+++ steam_api.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 
-class steam_api_functions:
+import urllib2
+
+
+class steam_api_connector():
+
+    steam_api_key = ''
+    steam_api_id = ''
+
+    def set_steam_api_key(self, api_key):
+        self.steam_api_key = api_key
+
+    def set_steam_api_id(self, api_id):
+        self.steam_api_id = api_id
+
+    def get_owned_games(self):
+        response = urllib2.urlopen("""
+        http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json
+        """ % (self.steam_api_key, self.steam_api_id))
+        return response.read()
