cb12cacf8adeb692f5243d10df2e9b439357b0b0
graydon-armstrong <seeds77222@hotmail.com>
2015-02-19 23:18:56 -0500

    Setup Basic Testing Steam API Testing page
    Just a basic page with api_key and a coded in user id that returns the
    owned games

--- index.py
+++ index.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python
 
-import cgi_header
-import urllib2
+from cgi_header import print_cgi_header
+import steam_api
 
-cgi_header.print_cgi_header()
+print_cgi_header()
 
-print '<h1>TEST Title</h1>'
-print '<p>Test LORUM IPSUM TEST</p>'
+print '<h1>Steam API Testing Page</h1>'
+print '<h2>Get Owned Games</h2>'
 
-listtest = ['1', '2', '3', '4']
+steam_api_connection = steam_api.steam_api_connector()
+steam_api_connection.set_steam_api_key('AF4B3CC7FBF9CD34127CE10E6CCA9B62')
+steam_api_connection.set_steam_api_id('76561197960434622')
 
-for item in listtest:
-    print '<p>' + item + '</p>'
+user_games_json = steam_api_connection.get_owned_games()
 
-
-steam_api_key = 'AF4B3CC7FBF9CD34127CE10E6CCA9B62'
-steam_id = '76561197960434622'
-response = urllib2.urlopen("""
-    http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json
-    """ % (steam_api_key, steam_id))
-html = response.read()
-print html
-
-# import steamapi
-
-# steamapi.core.APIConnection(api_key="AF4B3CC7FBF9CD34127CE10E6CCA9B62")
-# steamapi.user.SteamUser(userurl="smileybarry")
-
-# print 'test2'
+print user_games_json
