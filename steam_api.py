05a8c928c390164575c048785d31bd337b272c26
graydon-armstrong <seeds77222@hotmail.com>
2015-03-27 23:07:50 -0400

    Include more game info in return

--- steam_api.py
+++ steam_api.py
@@ -11,14 +11,16 @@ class steam_api_connector():
     steam_api_id = ''
 
     def set_steam_api_key(self, api_key):
+        """Set the api key"""
         self.steam_api_key = api_key
 
     def set_steam_api_id(self, api_id):
+        """Set the api id"""
         self.steam_api_id = api_id
 
     def get_owned_games(self):
         response = urllib2.urlopen("""
-        http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json
+        http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&include_appinfo=1&format=json
         """ % (self.steam_api_key, self.steam_api_id))
         json_string = response.read()
         return self.json_to_dictionary(json_string)['response']
@@ -28,5 +30,5 @@ class steam_api_connector():
         decoded_json = json.loads(json_string)
         return decoded_json
 
-    def get_gamename(self, appid):
+    def get_game_name(self, appid):
         pass
