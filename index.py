18e64ca3778dd75b50842bb066fca48213a69f4f
graydon-armstrong <seeds77222@hotmail.com>
2015-04-06 21:55:07 -0400

    Moved code to steam_games_dict functionality

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from cgi_header import print_cgi_header
-import steam_api
+from steam_games_dict import steam_games_dict
 
 
 def main():
@@ -13,35 +13,12 @@ def main():
     key = 'AF4B3CC7FBF9CD34127CE10E6CCA9B62'
     steam_id = '76561197989244442'
 
-    steam_api_connection = steam_api.steam_api_connector()
-    steam_api_connection.set_steam_api_key(key)
-    steam_api_connection.set_steam_api_id(steam_id)
+    steam_games = steam_games_dict(key, steam_id)
 
-    get_owned_games_dict = steam_api_connection.get_owned_games()
-    number_of_games_owned = get_owned_games_dict['game_count']
-    games_dict = get_owned_games_dict['games']
+    # print get_owned_games_dict
+    print 'Games owned: %s' % steam_games.get_number_of_games()
 
-    print get_owned_games_dict
-    print 'Games owned: %s' % number_of_games_owned
-
-    print_all_games(games_dict)
-
-
-def print_top_played_games(games_dict):
-    pass
-
-
-def print_all_games(games_dict):
-    print '<TABLE>'
-    for game in games_dict:
-        game['name'] = game['name'].encode('ascii', 'ignore')
-        print '<TR>'
-        print '<TD>Appid: %s</TD>' % game['appid']
-        print '<TD>Game Name: %s</TD>' % game['name']
-        print '<TD>Playtime: %s</TD>' % game['playtime_forever']
-        print '</TR>'
-
-    print '</TABLE>'
+    steam_games.print_all_games()
 
 if __name__ == '__main__':
     main()
