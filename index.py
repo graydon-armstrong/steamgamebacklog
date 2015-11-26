eaf64da76e0e138dfeefa925c04464f337b8b946
graydon-armstrong <seeds77222@hotmail.com>
2015-11-26 17:25:55 -0500

    Use the new page setup functions and use the default steam api key. Also update the imports to the new directory structure

--- index.py
+++ index.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 
-from cgi_header import print_cgi_header
-from steamgamesdict import SteamGamesDict
+from python.steamgamesdict import *
+from python.pagesetup import *
 
 
 def main():
     print_cgi_header()
+    print_html_start_tag()
+    print_head("TESTING 123")
 
-    print('<h1>Steam API Testing Page</h1>')
+    print('<h1>Steam Game Backlog</h1>')
     print('<h2>Get Owned Games</h2>')
 
-    key = 'AF4B3CC7FBF9CD34127CE10E6CCA9B62'
     steam_id = '76561197989244442'
 
-    steam_games = SteamGamesDict(key, steam_id)
+    steam_games = SteamGamesDict(steam_id)
 
     # print get_owned_games_dict
     print('Games owned: %s' % steam_games.get_number_of_games())
 
     steam_games.print_all_games()
+
+    print_html_end_tag()
 
 if __name__ == '__main__':
     main()
