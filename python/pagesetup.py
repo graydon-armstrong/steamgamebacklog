586a3586ca4580089e57209db2d62a3fc3e5b2cb
graydon-armstrong <seeds77222@hotmail.com>
2015-12-02 12:14:31 -0500

    Adding more of the page setup including body tags, the pages header, the nav element, and closing off the tags at the end of the web page

--- python/pagesetup.py
+++ python/pagesetup.py
@@ -12,14 +12,14 @@ def print_head(title=""):
     :param title: The title to be displayed in the title bar or tab of the browser
     """
     print """
-        <head>
+    <head>
         <meta charset="UTF-8">
         <meta name="description" content="Manage your Steam game backlog">
         <meta name="keywords" content="Steam,PC,Backlog,Games">
         <meta name="author" content="Graydon Armstrong">
         <title>%s</title>
-        </head>
-        """ % title
+    </head>
+    """ % title
 
 
 def print_doctype():
@@ -36,15 +36,55 @@ def print_html_end_tag():
     """Print the html closing tag"""
     print "</html>"
 
-def print_header():
+
+def print_page_header():
     """Print the header"""
-    print'<h1>Steam Game Backlog</h1>'
+    print """
+    <header>
+        <h1>Steam Game Backlog</h1>
+    </header>
+    """
+
+
+def print_body_start_tag():
+    """The pages Body start tag"""
+    print "<body>"
+
+
+def print_body_end_tag():
+    """The pages body end tag"""
+    print "</body>"
+
+
+def print_nav():
+    """Print the websites nav section"""
+    print """
+    <nav>
+        <ul>
+            <li><a href="index.py">Home</a></li>
+            <li><a href="index.py">Home</a></li>
+            <li><a href="index.py">Home</a></li>
+            <li><a href="index.py">Home</a></li>
+            <li><a href="index.py">Home</a></li>
+        </ul>
+    </nav>
+    """
 
 
 def setup_page(head_title="Steam Game Backlog"):
-    """Setup the start of the html page"""
+    """Setup the start of the html page
+    :param head_title: The title to be displayed in the title bar or tab of the browser
+    """
     print_cgi_header()
     print_doctype()
     print_html_start_tag()
     print_head(head_title)
-    print_header()
+    print_body_start_tag()
+    print_page_header()
+    print_nav()
+
+
+def close_page_tags():
+    """Close off the tags from the page setup"""
+    print_body_end_tag()
+    print_html_end_tag()
